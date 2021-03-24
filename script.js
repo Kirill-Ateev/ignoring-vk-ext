15aeb9d40c659138810915b15a1490a5a3c8677d
BGDSHKA <kirill.ateev59@gmail.com>
2021-03-24 12:56:48 +0300

    new version with MutationObserver

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Version 0.1
 // Change 17382564 to your toxic vk user
 //TODO: add event listeners to reduce memory usage
 setInterval(function(){ let elem = document.querySelectorAll('[data-peer="17382564"]')
