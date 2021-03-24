b1179a7ca1c9ca2c20206b2e729038de1239186f
BGDSHKA <kirill.ateev59@gmail.com>
2021-03-24 16:37:14 +0300

    add v2

---
+++ observer.js
@@ -0,0 +1,26 @@
+//Version 0.7
+let target = document.querySelector(".im-page--chat-body");
+
+// Конфигурация observer (за какими изменениями наблюдать)
+const config = {
+    childList: true,
+    subtree: true
+};
+
+// Колбэк-функция при срабатывании мутации
+const callback = function(mutationsList, observer) {
+    for (let mutation of mutationsList) {
+       if (mutation.type === 'childList') {
+let elem = document.querySelectorAll('[data-peer="17382564"]')
+            Array.prototype.forEach.call( elem, function( node ) {
+node.parentNode.removeChild( node );
+});
+
+}}
+};
+
+// Создаём экземпляр наблюдателя с указанной функцией колбэка
+const observer = new MutationObserver(callback);
+
+// Начинаем наблюдение за настроенными изменениями целевого элемента
+observer.observe(target, config);
