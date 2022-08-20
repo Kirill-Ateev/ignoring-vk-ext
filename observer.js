16bac157bbdac407c3df5d43839d6c55bf9dff2c
Kirill Ateev <kirill.ateev59@gmail.com>
2022-08-20 12:34:19 +0300

    feat: include shares from A. K.

--- observer.js
+++ observer.js
@@ -3,18 +3,20 @@ let target = document.querySelector(".im-page--chat-body");
 
 // Конфигурация observer (за какими изменениями наблюдать)
 const config = {
-    childList: true,
-    subtree: true
+  childList: true,
+  subtree: true,
 };
 
 // Колбэк-функция при срабатывании мутации
-const callback = function(mutationsList, observer) {
-    for (let mutation of mutationsList) {
-let elem = document.querySelectorAll('[data-peer="17382564"]')
-            Array.prototype.forEach.call( elem, function( node ) {
-node.parentNode.removeChild( node );
-});
-}
+const callback = function (mutationsList, observer) {
+  for (let mutation of mutationsList) {
+    const vdovin = document.querySelectorAll('[data-peer="17382564"]');
+    const shares = document.querySelectorAll('[post_view_hash="1f6aa1e2eff7ae5715"]');
+
+    Array.prototype.forEach.call([...vdovin, ...shares], function (node) {
+      node.parentNode.removeChild(node);
+    });
+  }
 };
 
 // Создаём экземпляр наблюдателя с указанной функцией колбэка
